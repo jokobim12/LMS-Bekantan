b85e9cc5c6594a2fe4c64ed634706346c901375d
jokobim12 <jokobimantaro88@gmail.com>
2025-09-02 18:52:09 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -366,13 +366,13 @@
                 <p class="subtitle">Masuk ke akun Anda</p>
             </div>
 
-            <form action="{{ route('dashboard') }}" method="POST">
+            <form action="{{ route('login') }}" method="POST">
                 @csrf
                 
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input 
-                        type="email" 
+                        type="email"    
                         id="email" 
                         name="email" 
                         placeholder="Masukkan email Anda"

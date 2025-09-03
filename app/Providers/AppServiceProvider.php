be3ef268d21a6b32b10b261bbf3c6c468101b6be
ChandraRGB <gustichandra1997@gmail.com>
2025-09-03 15:18:05 +0800

    penyesuaian app service untuk role admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,18 +8,12 @@ use App\Http\Responses\Auth\LogoutResponse as CustomLogoutResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        // Override response logout bawaan Filament
+        // Pakai custom logout response
         $this->app->bind(LogoutResponse::class, CustomLogoutResponse::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
